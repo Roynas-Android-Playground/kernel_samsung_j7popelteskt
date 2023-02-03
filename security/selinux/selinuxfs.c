dda527772a866de6e4363e3007987c6416652df7
roynatech2544 <whiteshell2544@naver.com>
2023-02-03 15:02:20 +0000

    SELinux: Allow overriding setenforce calls

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -178,7 +178,11 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 			new_value, selinux_enforcing,
 			from_kuid(&init_user_ns, audit_get_loginuid(current)),
 			audit_get_sessionid(current));
+#ifdef CONFIG_SECURITY_SELINUX_SETENFORCE_OVERRIDE_VALUE
+		selinux_enforcing = CONFIG_SECURITY_SELINUX_SETENFORCE_OVERRIDE_VALUE;
+#else
 		selinux_enforcing = new_value;
+#endif
 		if (selinux_enforcing)
 			avc_ss_reset(0);
 		selnl_notify_setenforce(selinux_enforcing);
