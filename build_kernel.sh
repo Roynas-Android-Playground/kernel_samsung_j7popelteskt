ceecce0697f46fb88dc4f52a27bfa43640a86cea
Soo Hwan Na <whiteshell2544@naver.com>
2024-01-21 23:43:37 +0900

    build_kernel.sh: Drop lld, fix config copy

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,8 +4,8 @@ export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 export PATH=$PWD/toolchain/bin:$PATH
 export ANDROID_MAJOR_VERSION=o
-COMMON="O=out CC=clang LD=ld.lld OBJDUMP=llvm-objdump OBJCOPY=llvm-objcopy AR=llvm-ar AS=llvm-as"
+COMMON="CC=clang OBJDUMP=llvm-objdump OBJCOPY=llvm-objcopy AR=llvm-ar AS=llvm-as"
 
 make $COMMON exynos7870-j7popeltekor_defconfig
-cp out/.config arch/arm64/configs/exynos7870-j7popeltekor_defconfig
+cp .config arch/arm64/configs/exynos7870-j7popeltekor_defconfig
 make $COMMON -j$(nproc)
