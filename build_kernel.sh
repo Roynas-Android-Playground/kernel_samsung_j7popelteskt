cafd15493f13785818de32861ddd92946fac6907
Soo Hwan Na <whiteshell2544@naver.com>
2024-01-23 00:57:59 +0900

    build_kernel: Switch to defconfig

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,5 +7,6 @@ export ANDROID_MAJOR_VERSION=o
 COMMON="CC=clang OBJDUMP=llvm-objdump OBJCOPY=llvm-objcopy AR=llvm-ar AS=llvm-as"
 
 make $COMMON exynos7870-j7popeltekor_defconfig
-cp .config arch/arm64/configs/exynos7870-j7popeltekor_defconfig
+make $COMMON savedefconfig
+cp defconfig arch/arm64/configs/exynos7870-j7popeltekor_defconfig
 make $COMMON -j$(nproc)
