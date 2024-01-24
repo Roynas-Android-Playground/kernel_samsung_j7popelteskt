7aac17465e65f347ba734fef38651e1a5c0e606d
Soo Hwan Na <whiteshell2544@naver.com>
2024-01-24 14:50:29 +0900

    build_kernel: Target Android P

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,7 +3,7 @@
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 export PATH=$PWD/toolchain/bin:$PATH
-export ANDROID_MAJOR_VERSION=o
+export ANDROID_MAJOR_VERSION=p
 COMMON="CC=clang OBJDUMP=llvm-objdump OBJCOPY=llvm-objcopy AR=llvm-ar AS=llvm-as"
 
 make $COMMON exynos7870-j7popeltekor_defconfig
